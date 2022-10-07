956dc70a9749c066b8574470fafc41af9d0a7348
lavagancia <potola30@gmail.com>
2022-10-06 21:01:17 -0400

    updated

--- scrip.js
+++ scrip.js
@@ -10,3 +10,13 @@ function closeImg(){
     fulImgBox.style.display = "none";
 }
     
+
+function toggleVideo(){
+const trailer= document.querySelector(".trailer");
+const video = document.querySelector("video");
+trailer.classList.toggle("active");
+video.currentTime=0;
+video.pause();
+
+ }
+
